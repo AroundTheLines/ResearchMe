4a8f664ca5ab34bff46c1549c8e5d2fca7f7c72e
Qile Wang <wangqile123@gmail.com>
2015-11-08 06:29:48 -0500

    Added some css to everything

--- YHack/settings.py
+++ YHack/settings.py
@@ -100,4 +100,5 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_ROOT = '/Users/qilewang/Documents/GithubRepos/YHack2015/static'
+STATIC_URL = '../static/'
